dfce8c8899a690ea37e1bf84f23afe39a40a7d10
“1626216058@qq.com” <1626216058@qq.com>
2024-10-20 17:12:22 +0800

    Test Solution18

---
+++ TestSolution18.java
@@ -0,0 +1,7 @@
+public class TestSolutioned {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-1,1,0,-3,3};
+        int[] result = solution.productExceptSelf(nums);
+    }
+}
